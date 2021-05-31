773bdcd5f6a8fe5038a523fd5041d2bf8e134a18
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2021-05-31 09:43:51 -0400

    added frontend sumbodule

--- opentera-webrtc-demos/scripts/start_signaling_server.sh
+++ opentera-webrtc-demos/scripts/start_signaling_server.sh
@@ -8,6 +8,6 @@ static_folder=$4
 echo "port :" $port
 echo "password:" $password
 echo "static_folder:" $static_folder
-echo "Client interface will be available at http://localhost:$port/index.html?pwd=$password"
+echo "Client interface will be available at http://localhost:$port/index.html#/user?pwd=$password"
 
 (cd $1; python3 -m pip install -r requirements.txt;python3 signaling_server.py --port $port --password $password --static_folder $static_folder)
