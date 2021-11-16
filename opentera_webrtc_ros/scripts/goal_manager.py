cffc3d3b9c166c983ecac039dce3e9cc6f0200aa
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2021-11-16 13:02:18 -0500

    Cancel docking

--- opentera_webrtc_ros/scripts/goal_manager.py
+++ opentera_webrtc_ros/scripts/goal_manager.py
@@ -14,9 +14,7 @@ from std_msgs.msg import Bool, String
 from std_srvs.srv import SetBool
 
 class GoalManager():
-    def __init__(self):
-        rospy.init_node("goal_manager")        
-
+    def __init__(self):  
         self.dr_client = dynamic_reconfigure.client.Client("/move_base/DWAPlannerROS")
 
         # Global action client used to send waypoints to move_base
@@ -28,12 +26,13 @@ class GoalManager():
         # Subscribers and publishers
         self.waypoints_sub = rospy.Subscriber("waypoints", WaypointArray, self.waypoints_cb)
         self.stop_sub = rospy.Subscriber("stop", Bool, self.stop_cb)
-        self.dock_action_sub = rospy.Subscriber("dock_action", Bool, self.dock_cb)
         self.pre_docking_pose_sub = rospy.Subscriber("pre_docking_pose", PoseStamped, self.pre_docking_pose_cb)
         self.waypoint_reached_pub = rospy.Publisher("waypoint_reached", String, queue_size=1)
         self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
         self.should_stop = False
+
+        rospy.loginfo("Goal manager ready")
 
     def waypoints_cb(self, msg):
         self.move_base_client.cancel_all_goals()
@@ -69,7 +68,7 @@ class GoalManager():
             res = image_goal_to_map_goal(image_goal)
             return res.map_goal
         except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+            rospy.logwarn("Service call failed: %s"%e)
 
     def send_goal(self, pose_goal):
         goal = MoveBaseGoal()
@@ -78,15 +77,9 @@ class GoalManager():
         self.move_base_client.wait_for_result()
 
     def stop_cb(self, msg):
+        rospy.loginfo("Stopping")
         if msg.data == True:
             self.should_stop = True
-            self.move_base_client.cancel_all_goals()
-            self.clearGlobalPathClient()
-
-    def dock_cb(self, msg):
-        if msg.data == True:
-            self.send_goal(self.dock_pose)
-        else:
             self.move_base_client.cancel_all_goals()
             self.clearGlobalPathClient()
 
@@ -101,40 +94,43 @@ class GoalManager():
             clear_global_path = rospy.ServiceProxy('clear_global_path', SetBool)
             clear_global_path(True)
         except rospy.ServiceException as e:
-            print("Service call failed: %s" % e)
+            rospy.logwarn("Service call failed: %s" % e)
     
     def pre_docking_pose_cb(self, pose):
         # Need to change goal tolerances to something small to make sure the docking is precise.
-        print("Got pre-docking pose")
+        rospy.loginfo("Got pre-docking pose")
         prev_dwa_config = self.dr_client.get_configuration()
         new_dwa_config = prev_dwa_config.copy()
-        new_dwa_config["xy_goal_tolerance"] = 0.05
+        new_dwa_config["xy_goal_tolerance"] = 0.06
         new_dwa_config["yaw_goal_tolerance"] = 0.0349066
         self.dr_client.update_configuration(new_dwa_config)
-        print("Changed goal tolerances")
-        print("Sending goal to move_base")
+        rospy.loginfo("Changed goal tolerances")
+        rospy.loginfo("Sending goal to move_base")
+        self.should_stop = False
         self.send_goal(pose)
         # Reset DWA config to what it was previously
         self.dr_client.update_configuration(prev_dwa_config)
 
         # Go backwards until connected to charger
         # TODO: refactor. Should this be handled by move_base? If not should it be in docking.py instead?
-        cmd = Twist()
-        cmd.linear.x = -0.1
-        r = rospy.Rate(10)
-        start_time = rospy.Time.now()
-        elapsed_time = 0
-        max_time = 3
-        print("Backing for %.1f seconds" % max_time)
-        # TODO: find better way of knowing when to stop backing up
-        while not rospy.is_shutdown() and elapsed_time < max_time:
-            self.cmd_vel_pub.publish(cmd)
-            elapsed_time = (rospy.Time.now() - start_time).to_sec()
-            r.sleep()
+        # TODO: should not use timer for determining when to stop backing up
+        if not self.should_stop:
+            cmd = Twist()
+            cmd.linear.x = -0.1
+            r = rospy.Rate(10)
+            start_time = rospy.Time.now()
+            elapsed_time = 0
+            max_time = 3
+            rospy.loginfo("Backing for %.1f seconds" % max_time)
+            # TODO: find better way of knowing when to stop backing up
+            while not rospy.is_shutdown() and elapsed_time < max_time:
+                self.cmd_vel_pub.publish(cmd)
+                elapsed_time = (rospy.Time.now() - start_time).to_sec()
+                r.sleep()
         
 
 if __name__ == '__main__':
-    print("Goal manager ready")
+    rospy.init_node("goal_manager")
     try:
         goal_manager = GoalManager()
         rospy.spin()
