e52ee7f72583c62901a29ae4d261eeb6ecda3aa4
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2021-11-17 14:43:15 -0500

    Return move_base goal reached status

--- opentera_webrtc_ros/scripts/goal_manager.py
+++ opentera_webrtc_ros/scripts/goal_manager.py
@@ -8,6 +8,7 @@ from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import PoseStamped, Twist
 from opentera_webrtc_ros_msgs.msg import WaypointArray
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from actionlib_msgs.msg._GoalStatus import GoalStatus
 from map_image_generator.srv import ImageGoalToMapGoal
 from std_msgs.msg import Bool, String
 from std_srvs.srv import SetBool
@@ -22,6 +23,8 @@ class GoalManager():
         self.move_base_client.wait_for_server()
         # Make sure no goals are active
         self.move_base_client.cancel_all_goals()
+
+        self.should_stop = False
 
         # Subscribers and publishers
         self.waypoints_sub = rospy.Subscriber("waypoints", WaypointArray, self.waypoints_cb)
@@ -73,6 +76,7 @@ class GoalManager():
         goal.target_pose = pose_goal
         self.move_base_client.send_goal(goal)
         self.move_base_client.wait_for_result()
+        return self.move_base_client.get_state()
 
     def stop_cb(self, msg):
         rospy.loginfo("Stopping")
@@ -99,16 +103,19 @@ class GoalManager():
         rospy.loginfo("Got pre-docking pose")
         prev_dwa_config = self.dr_client.get_configuration()
         new_dwa_config = prev_dwa_config.copy()
-        new_dwa_config["xy_goal_tolerance"] = 0.05
+        new_dwa_config["xy_goal_tolerance"] = 0.1
         new_dwa_config["yaw_goal_tolerance"] = 0.0349066
         self.dr_client.update_configuration(new_dwa_config)
         rospy.loginfo("Changed goal tolerances")
         rospy.loginfo("Sending goal to move_base")
         self.should_stop = False
-        self.send_goal(pose)
+        state = self.send_goal(pose)
         # Reset DWA config to what it was previously
         self.dr_client.update_configuration(prev_dwa_config)
-        self.pre_docking_pose_reached_pub.publish(Bool(True))
+        if state == GoalStatus.SUCCEEDED:
+            self.pre_docking_pose_reached_pub.publish(Bool(True))
+        else:
+            self.pre_docking_pose_reached_pub.publish(Bool(False))
 
         
 if __name__ == '__main__':
