74e01fc80b6a4cad59361f3557fe2fd7f48a205b
philippewarren <philippewarren31@gmail.com>
2022-02-24 13:35:21 -0500

    Added support for ttop

--- opentera_webrtc_demos/scripts/start_signaling_server.sh
+++ opentera_webrtc_demos/scripts/start_signaling_server.sh
@@ -1,13 +1,17 @@
-#!/bin/bash          
+#!/bin/bash
 
-echo "Starting opentera signaling server from :" $1 
+echo "Starting opentera signaling server from :" $1
 port=$2
 password=$3
 static_folder=$4
+robot_type=$5
+python_exec=$6
 
 echo "port :" $port
 echo "password:" $password
 echo "static_folder:" $static_folder
-echo "Client interface will be available at http://localhost:$port/index.html#/user?pwd=$password"
+echo "robot_type:" $robot_type
+echo "Client interface will be available at http://localhost:$port/index.html#/user?pwd=$password&robot=$robot_type"
 
-(cd $1; python3 -m pip install -r requirements.txt;python3 signaling_server.py --port $port --password $password --static_folder $static_folder)
+
+(cd $1; $python_exec -m pip install -r requirements.txt; $python_exec signaling_server.py --port $port --password $password --static_folder $static_folder --robot_type $robot_type)
