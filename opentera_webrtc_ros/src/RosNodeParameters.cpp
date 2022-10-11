430f35652d9835d7b62822c533a670a31bc4297f
philippewarren <philippewarren31@gmail.com>
2022-10-11 11:05:32 -0400

    Fix API for new version of libwebrtc

--- opentera_webrtc_ros/src/RosNodeParameters.cpp
+++ opentera_webrtc_ros/src/RosNodeParameters.cpp
@@ -108,8 +108,6 @@ void RosNodeParameters::loadAudioStreamParams(
     bool& noiseSuppression,
     bool& highPassFilter,
     bool& stereoSwapping,
-    bool& typingDetection,
-    bool& residualEchoDetector,
     bool& transientSuppression)
 {
     NodeHandle pnh("~");
@@ -130,7 +128,5 @@ void RosNodeParameters::loadAudioStreamParams(
     noiseSuppression = isInParams("noise_suppression", dict) ? dict["noise_suppression"] : true;
     highPassFilter = isInParams("high_pass_filter", dict) ? dict["high_pass_filter"] : false;
     stereoSwapping = isInParams("stereo_swapping", dict) ? dict["stereo_swapping"] : false;
-    typingDetection = isInParams("typing_detection", dict) ? dict["typing_detection"] : false;
-    residualEchoDetector = isInParams("residual_echo_detector", dict) ? dict["residual_echo_detector"] : true;
     transientSuppression = isInParams("transient_suppression", dict) ? dict["transient_suppression"] : true;
 }
