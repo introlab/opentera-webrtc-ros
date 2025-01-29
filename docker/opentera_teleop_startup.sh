908359bd256fa6eb16d3489e82b3e324a49745ed
WENDWOSEN BEDADA <visitwende@gmail.com>
2025-01-29 12:21:28 -0600

    certificate generated and installed inside container. clean up unecessary lines and files. added missing blank spaces.

--- docker/opentera_teleop_startup.sh
+++ docker/opentera_teleop_startup.sh
@@ -1,14 +1,20 @@
+#set rmw and source workspace
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 source /opt/ros/OPENTERA_WS/opentera-webrtc-ros/install/setup.bash
+
+# Add /usr/lib/x86_64-linux-gnu to the library path
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+
+# Add /usr/local/lib to the library path
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib 
+
+# Set the Video Acceleration API (VA-API) driver to NVIDIA
 export LIBVA_DRIVER_NAME=nvidia
+
 # Add the signaling server to PATH
 export PATH=/opt/ros/OPENTERA_WS/opentera-webrtc-ros/install/opentera_webrtc_ros/local/bin:$PATH
-ros2 pkg prefix opentera_client_ros
-echo "PATH is: $PATH"
-sh -c "chmod 0700 /tmp/runtime-root"
 
+# source gazebo and add additional gazebo model paths
 source /usr/share/gazebo/setup.sh
 export PATH=$PATH:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins  # For Gazebo plugins
 export GAZEBO_MODEL_PATH=/opt/ros/OPENTERA_WS/opentera-webrtc-ros/install/opentera_webrtc_demos/share/opentera_webrtc_demos/models:$GAZEBO_MODEL_PATH
@@ -21,4 +27,3 @@ sudo sysctl -w net.core.rmem_max=2147483647
 
 echo $ROS_DISTRO
 ros2 launch  startup_opentera_demos.launch.py
-#ros2 run rviz2 rviz2
